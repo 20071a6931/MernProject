96c1c8e275cdacdf6735508ccf46f4e1bbba5e23
Udaykiran <koradaudaykiran1406@gmail.com>
2022-06-17 22:07:38 +0530

    mern stack application completed

---
+++ src/components/Products.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import React, { useState } from "react";
+import Product from "./Product";
+
+function Products() {
+  let [productsList, setProductsList] = useState([]);
+
+    axios
+    .get("http://localhost:4000/product-api/getproducts")
+    .then((response) => {
+      setProductsList(response.data.payload);
+    })
+
+  return (
+    <div className="text-center">
+      <p className="display-2 phead">Medicines</p>
+      <hr />
+      
+        <div className="d-flex card-group abc">
+          {productsList.map((productObj, index) => (
+            <Product key={index} productObj={productObj} />
+          ))}
+        </div>
+    </div>
+  );
+}
+
+export default Products;
---
+++ src/components/Productsone.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import React, { useState } from "react";
+import Productone from "./Productone";
+
+function Productsone() {
+  let [productsList, setProductsList] = useState([]);
+
+    axios
+    .get("http://localhost:4000/product-api/getproducts")
+    .then((response) => {
+      setProductsList(response.data.payload);
+    })
+
+  return (
+    <div className="text-center">
+      <p className="display-2 phead">Medicines</p>
+      <hr />
+      
+        <div className="d-flex card-group abc">
+          {productsList.map((productObj, index) => (
+            <Productone key={index} productObj={productObj} />
+          ))}
+        </div>
+    </div>
+  );
+}
+
+export default Productsone;
